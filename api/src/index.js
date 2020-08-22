61b361fa5ab0e6ac66a7abc679506a316d8dced7
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-22 16:55:49 +0200

    express||mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT;
-const host = process.env.HOST;
+const express = require("express");
+const mongoose = require("mongoose");
+const { host, port, db } = require("./configuration");
+const { connectDb } = require("./helpers/db");
+
+const app = express();
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api service on port:${port}`);
+    console.log(`On host ${host}`);
+    console.log(`Our database ${db}`);
+  });
+};
 
 app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
-app.listen(port, () => {
-  console.log(`Started api service on port:${port}`);
-  console.log(`On host ${host}`);
-});
-
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
