29e91a9619687f25134afb8af837f42898f4e571
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-08-23 09:57:38 +0200

    add Post

--- api/src/index.js
+++ api/src/index.js
@@ -2,14 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { host, port, db } = require("./configuration");
 const { connectDb } = require("./helpers/db");
-
 const app = express();
+const postSchema = new mongoose.Schema({
+  name: String
+});
+const Post = mongoose.model('Post', postSchema);
+
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Started api service on port:${port}`);
     console.log(`On host ${host}`);
     console.log(`Our database ${db}`);
+
+    // Post.find(function(err, posts) {
+    //   if (err) return console.error(err);
+    //   console.log('posts', posts);
+    // });
+
+    const silence = new Post({ name: "Silence"});
+    silence.save(function(err, savedSilence) {
+      if (err) return console.error(err);
+      console.log("savedSilence", savedSilence);
+    });
   });
 };
 
